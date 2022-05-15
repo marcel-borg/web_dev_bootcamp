491ebb690386e53e208f3cfcb63bd2c50d02b828
marcel-borg <mar_borg@hotmail.co.uk>
2022-05-15 19:17:11 +0100

    beginner js finished

--- beginner_javascript/js_intro.js
+++ beginner_javascript/js_intro.js
@@ -49,3 +49,103 @@ age ++; // adds 1
 age;
 age += 10; // adds 10
 age;
+
+// FUNCTIONS
+
+//var bottles = prompt("How many bottles of milk do you need?")
+var money = prompt("How many cash do you have to spend? ");
+var cost = 1.5;
+var bottles = money / cost;
+
+function getMilk(bottles) {   
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("You can buy " + Math.floor(bottles) + " bottles of milk")
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+}
+
+getMilk(bottles);
+
+// v2 with a change return
+var money = prompt("How many cash do you have to spend? ");
+var cost = 1.5;
+var bottles = money / cost;
+
+function getMilk(bottles) {   
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("You can buy " + Math.floor(bottles) + " bottles of milk")
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+    return money % cost // remainder of the division
+}
+
+getMilk(bottles);
+
+
+// time left until 90 years old calculator
+
+function lifeInWeeks(age) {
+    var age = prompt("How old are you? ");
+    var yearsRemaining = 90 - age;
+    var days = yearsRemaining * 365;
+    var weeks = yearsRemaining * 52;
+    var months = yearsRemaining * 12;
+    console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.");
+}
+
+lifeInWeeks();
+
+
+// BMI Calculator
+
+//Create your function below this line.
+//The first parameter should be the weight and the second should be the height.
+
+var weight = prompt("Enter your weight (in kg) ")
+var height = prompt("Enter your height (in m) ")
+
+function bmiCalculator(weight,height) {
+    var bmi = weight / Math.pow(height,2);
+    return bmi;
+}
+
+bmiCalculator(weight, height);
+
+
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+var bmi = bmiCalculator(65, 1.8); 
+
+bmi should equal 20 when it's rounded to the nearest whole number.
+
+*/
